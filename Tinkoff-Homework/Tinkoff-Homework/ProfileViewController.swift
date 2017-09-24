bc8494db9dfaa18462a024aaa7b3b17ba947d0c3
leoniknik <leoniknik@mail.ru>
2017-09-24 16:07:20 +0300

    состояния приложения

---
+++ Tinkoff-Homework/Tinkoff-Homework/ProfileViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tinkoff-Homework
+//
+//  Created by Кирилл Володин on 24.09.17.
+//  Copyright © 2017 Кирилл Володин. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+
+    }
+
+
+}
+
