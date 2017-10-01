1b34fab210b5a9f1b0797d38523c23543dff675f
leoniknik <leoniknik@mail.ru>
2017-10-01 15:13:11 +0300

    скругленные края

--- Tinkoff-Homework/Tinkoff-Homework/ProfileViewController.swift
+++ Tinkoff-Homework/Tinkoff-Homework/ProfileViewController.swift
@@ -10,13 +10,38 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var changeProfileImageButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        makeViewsCircular()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    func makeViewsCircular() {
+        let radius = changeProfileImageButton.frame.size.width/2
+        
+        changeProfileImageButton.layer.cornerRadius = radius
+        changeProfileImageButton.clipsToBounds = true
+        
+        profileImage.layer.cornerRadius = radius
+        profileImage.clipsToBounds = true
+    }
+    
+    @IBAction func changeProfileImage(_ sender: Any) {
+        print("Выбери изображение профиля")
+        
+    }
+    
 
 }
 
